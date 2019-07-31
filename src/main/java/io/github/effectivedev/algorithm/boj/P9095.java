3d00122f36955f4c98547008094a2214e8ecf635
Inyoung <in0@me.com>
2019-07-31 09:26:19 +0900

    add P9095

---
+++ src/main/java/io/github/effectivedev/algorithm/boj/P9095.java
@@ -0,0 +1,28 @@
+package io.github.effectivedev.algorithm.boj;
+
+import java.util.Scanner;
+
+public class P9095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        for (int i = 0; i < cnt; i++) {
+            int goal = sc.nextInt();
+            System.out.println(go(0, goal));
+        }
+    }
+
+    private static int go(int sum, int goal) {
+        if (sum > goal) {
+            return 0;
+        }
+        if (sum == goal) {
+            return 1;
+        }
+        int now = 0;
+        for (int i = 1; i <= 3; i++) {
+            now += go(sum + i, goal);
+        }
+        return now;
+    }
+}
