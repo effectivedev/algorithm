4db1979da5a69558d4e1e036cf56c1e4be224f1c
Inyoung <in0@me.com>
2019-08-06 15:31:09 +0900

    패키지 이동

---
+++ src/main/java/io/github/effectivedev/algorithm/boj/P6603.java
@@ -0,0 +1,27 @@
+package io.github.effectivedev.algorithm.boj;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+//lotto
+public class P6603 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String nextLine = sc.nextLine();
+            if ("0".equals(nextLine)) {
+                break;
+            }
+            List<Integer> list = Arrays.stream(nextLine.split("\\s")).map(i -> Integer.valueOf(i))
+                    .collect(Collectors.toList());
+            int k = list.get(0);
+            List<Integer> sublist = list.subList(1, list.size());
+            System.out.println(k);
+            System.out.println(sublist);
+        }
+    }
+
+}
---
+++ src/main/java/io/github/effectivedev/algorithm/boj/bruteforce/P2231.java
@@ -0,0 +1,8 @@
+package io.github.effectivedev.algorithm.boj.bruteforce;
+
+
+public class P2231 {
+    public static void main(String[] args) {
+
+    }
+}
