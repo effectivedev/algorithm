5024e09d73c687414ad5e88f2be4f7b4376f41c6
Inyoung <in0@me.com>
2019-08-12 11:01:37 +0900

    알고리즘 최적화

--- src/main/java/io/github/effectivedev/algorithm/boj/function/P11729.java
+++ src/main/java/io/github/effectivedev/algorithm/boj/function/P11729.java
@@ -12,26 +12,24 @@ import java.io.InputStreamReader;
  */
 
 public class P11729 {
-    static int cnt = 0;
     static StringBuilder sb = new StringBuilder();
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(hanoi(n,1,2,3));
+        System.out.println((1<<n) -1);
+        hanoi(n, 1,2,3);
         System.out.print(sb);
     }
 
-    private static int hanoi(int n, int start, int mid, int end ){
-        if(n == 1){
-            sb.append(start).append(" ").append(end).append("\n");
+    private static void hanoi(int n, int start, int mid, int end ){
+        if(n == 0){
+            return;
         }
         else{
             hanoi(n-1, start, end, mid);
             sb.append(start).append(" ").append(end).append("\n");
             hanoi(n-1, mid, start, end);
         }
-        cnt++;
-        return cnt;
     }
 }
 
