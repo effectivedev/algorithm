6643966661a945850168bfa0a073153e35d264c9
Inyoung <in0@me.com>
2019-08-22 17:24:54 +0900

    * 좋은 단어  * https://www.acmicpc.net/problem/3986

---
+++ src/main/java/io/github/effectivedev/algorithm/boj/string/P3986.java
@@ -0,0 +1,45 @@
+package io.github.effectivedev.algorithm.boj.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 좋은 단어
+ * https://www.acmicpc.net/problem/3986
+ * Github : https://github.com/effectivedev
+ * Created by in0@me.com on 2019-08-22 14:20
+ */
+
+public class P3986 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String line = null;
+        char[] word = null;
+        char[] result =null;
+
+        int cnt = 0;
+        for (int i = 0; i <n; i++) {
+            line = br.readLine();
+            word = line.toCharArray();
+            result = new char[100000];
+            int length = word.length;
+            int index = 0;
+            for (int j = 0; j < length; j++) {
+                result[index++] = word[j];
+                if (index >= 2) {
+                    if (((result[index - 1] == 'A') && (result[index - 2] == 'A')) || ((result[index - 1] == 'B') && (result[index - 2] == 'B'))) {
+                        result[index] = '\0';
+                        index = index - 2;
+                    }
+                }
+            }
+            if(index == 0){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
