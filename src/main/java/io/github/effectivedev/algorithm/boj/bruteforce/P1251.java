3aa2117b0a81ea807a2c0b8eb4f550288111340e
Inyoung <in0@me.com>
2019-09-06 18:02:05 +0900

    ing

--- src/main/java/io/github/effectivedev/algorithm/boj/bruteforce/P1251.java
+++ src/main/java/io/github/effectivedev/algorithm/boj/bruteforce/P1251.java
@@ -3,6 +3,10 @@ package io.github.effectivedev.algorithm.boj.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 단어 나누기
@@ -14,7 +18,35 @@ import java.io.InputStreamReader;
 public class P1251 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        char[] word = br.readLine().toCharArray();
+        int length = word.length;
+        int[] arr = new int[length];
+        List<String> list = new ArrayList<>();
+        combination(arr, length, 2, 0, 0, word, list);
+        Collections.sort(list);
+        System.out.println(list.get(0));
+    }
 
+    private static void combination(int[] arr, int n, int r, int index, int target, char[] result, List<String> list) {
+        if (r == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = arr[0]; i > -1 ; i--) {
+                sb.append(result[i]);
+            }
+            for (int i = arr[1]; i > arr[0] ; i--) {
+                sb.append(result[i]);
+            }
+            for (int i = result.length-1; i > arr[1] ; i--) {
+                sb.append(result[i]);
+            }
+            list.add(sb.toString());
+            return;
+        } else if (target == n) {
+            return;
+        } else {
+            arr[index] = target;
+            combination(arr, n, r - 1, index + 1, target + 1, result, list);
+            combination(arr, n, r, index, target + 1, result, list);
+        }
     }
 }
