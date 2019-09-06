617f6a927f082151b3d686b76b13c21ddd1c7aed
Inyoung <in0@me.com>
2019-09-06 18:01:44 +0900

    ing

---
+++ src/main/java/io/github/effectivedev/algorithm/boj/bruteforce/P1251.java
@@ -0,0 +1,20 @@
+package io.github.effectivedev.algorithm.boj.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 단어 나누기
+ * https://www.acmicpc.net/problem/1251
+ * Github : https://github.com/effectivedev
+ * Created by in0@me.com on 06/09/2019 3:53 오후
+ */
+
+public class P1251 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
