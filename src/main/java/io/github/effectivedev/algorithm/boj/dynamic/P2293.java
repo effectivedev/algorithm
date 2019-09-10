8094c89e8445dae92774de46938fff5dc30adc47
Inyoung <in0@me.com>
2019-09-11 08:48:07 +0900

    * 동전 1  * https://www.acmicpc.net/problem/2293

--- src/main/java/io/github/effectivedev/algorithm/boj/dynamic/P2293.java
+++ src/main/java/io/github/effectivedev/algorithm/boj/dynamic/P2293.java
@@ -3,7 +3,6 @@ package io.github.effectivedev.algorithm.boj.dynamic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -17,17 +16,21 @@ public class P2293 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int arr[] = new int[n+1];
-        for (int i = 1; i <=n ; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N+1];
+        int[] DP = new int[K+1];
+        for (int i = 1; i <= N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
-        int DP[] = new int[k+1];
-        DP[1] = 1;
-        DP[2] = 2;
-        DP[3] = DP[1]+DP[2]- DP[2];
-
-
+        DP[0] = 1;
+        for (int i = 1; i <= N ; i++) {
+            for (int j = 1; j <= K; j++) {
+                if(j >= coins[i]){
+                    DP[j] += DP[j-coins[i]];
+                }
+            }
+        }
+        System.out.printf("%d\n", DP[K]);
     }
 }
