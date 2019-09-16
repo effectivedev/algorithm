ff519a1236216b1e45400c987d0aeaaed82b1248
Inyoung <in0@me.com>
2019-09-16 16:07:50 +0900

    * N과 M (1)  * https://www.acmicpc.net/problem/15649

---
+++ src/main/java/io/github/effectivedev/algorithm/boj/bruteforce/P15649.java
@@ -0,0 +1,47 @@
+package io.github.effectivedev.algorithm.boj.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * N과 M (1)
+ * https://www.acmicpc.net/problem/15649
+ * Github : https://github.com/effectivedev
+ * Created by in0@me.com on 16/09/2019 3:28 오후
+ */
+
+public class P15649 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        permutation(N, M, new Stack<Integer>(), sb);
+        System.out.print(sb);
+    }
+
+    private static void permutation(int n, int r, Stack<Integer> st, StringBuilder sb) {
+        if (r == 0) {
+            for (int i = 0; i < st.size(); i++) {
+                sb.append(st.get(i));
+                if (i != st.size() - 1) {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+            return;
+        } else {
+            for (int i = 1; i < n + 1; i++) {
+                if (!st.contains(i)) {
+                    st.push(i);
+                    permutation(n, r - 1, st, sb);
+                    st.pop();
+                }
+            }
+        }
+    }
+}
